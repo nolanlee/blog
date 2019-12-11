fcc83519033a207445a84d096a20d0e4393cc115
nolanlee <nosaiforever@gmail.com>
2019-12-11 20:22:19 +0800

    hello,wasm

--- bin/convertImg.js
+++ bin/convertImg.js
@@ -1,17 +1,18 @@
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
+const imagemin = require('imagemin')
+const imageminWebp = require('imagemin-webp')
 
-const argv = process.argv;
-const src = argv[2];
-const target = argv[3];
+const argv = process.argv
+const src = argv[2]
+const target = argv[3]
 
-
-(async () => {
+;(async () => {
   const files = await imagemin([`${src}*.{jpg,png}`], {
     destination: target,
-    plugins: [
-      imageminWebp()
-    ]
-  });
-  console.log(`Images optimized: \n${files.map(item => item.destinationPath).join('\n')}`);
-})();
+    plugins: [imageminWebp()]
+  })
+  console.log(
+    `Images optimized: \n${files
+      .map((item) => item.destinationPath)
+      .join('\n')}`
+  )
+})()
